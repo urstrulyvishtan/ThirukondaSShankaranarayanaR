17aa8d16cb583678e64b8a41c87412ea92003e96
Ravi Shankar <ivarraknash3@gmail.com>
2023-04-19 20:08:01 -0400

    Admin create job completed

---
+++ admin_operations.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 19 06:58:11 2023
+
+@author: ivarr
+"""
+from create_job import createJob
+
+def adminOperations(admin_username,cnx):
+    
+    while(True):
+        cursor = cnx.cursor()
+        emp_id_temp = "SELECT employer_id FROM employer WHERE user_name = %s"
+        emp_id = cursor.execute(emp_id_temp, admin_username)
+        print("Welcome " + emp_id + "! \n" )
+        print("1. Create Applications \n")
+        print("2. Update Application status \n")
+        print("3. Logout \n")
+        admin_operation = int(input("Enter a number from the above menu:"))
+        if(admin_operation == 1):
+           flag = createJob(admin_username, cnx) 
+           if(flag == 1):
+               continue
+        
+        elif(admin_operation == 2):
+           print("TBD")
+        
+        elif(admin_operation == 3):
+           return admin_operation
